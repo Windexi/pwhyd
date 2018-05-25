65a2b8d331b981919fa4972bcb2e076ca98a0b9a
Windexi <jonahbrawley@gmail.com>
2018-05-25 02:28:22 -0700

    Let's put the coordinates in the top left and see what happens.

--- pokecrystal-source/maps/NewBarkTown.asm
+++ pokecrystal-source/maps/NewBarkTown.asm
@@ -289,6 +289,8 @@ NewBarkTown_MapEvents:
 	warp_event 11, 13, ELMS_HOUSE, 1
 
 	db 2 ; coord events
+	coord_event  0,  0, SCENE_DEFAULT, NewBarkTown_TeacherStopsYouScene1
+	coord_event  0,  0, SCENE_DEFAULT, NewBarkTown_TeacherStopsYouScene2
 
 	db 4 ; bg events
 	bg_event  8,  8, BGEVENT_READ, NewBarkTownSign
