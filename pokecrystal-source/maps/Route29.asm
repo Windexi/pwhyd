6e4bee02d4519122f55284e45f977523a1cd768d
Windexi <jonahbrawley@gmail.com>
2018-10-15 15:01:15 -0700

    Added druggie lounge on R29

--- pokecrystal-source/maps/Route29.asm
+++ pokecrystal-source/maps/Route29.asm
@@ -20,8 +20,8 @@ Route29Sign1:
 Route29Sign2:
 	jumptext Route29Sign2Text
 
-;Route29HiddenItem:
-;	hiddenitem RARE_CANDY, EVENT_ROUTE29_HIDDEN_ITEM
+Route29HiddenItem:
+	hiddenitem RARE_CANDY, EVENT_ROUTE29_HIDDEN_ITEM
 
 MrLetsBeFriends:
 	faceplayer
@@ -41,9 +41,35 @@ MrLetsBeFriendsMovement:
 	step UP
 	step_end
 
+Bouncer:
+	faceplayer
+	opentext
+	writetext BouncerText
+	waitbutton
+	closetext
+	end
+
+BouncerFriend1:
+	; in future event
+	end
+
+BouncerFriend2:
+	; in future event
+	end
+
+BouncerText:
+	text "Hey kid!"
+
+	para "This place is"
+	line "for me and my"
+	cont "OGs only!"
+
+	para "Now scram!"
+	done
+
 MrLetsBeFriendsText:
-	text "Well hey there young"
-	line "man."
+	text "Well hey there"
+	line "young man..."
 
 	para "Are you lost?"
 
@@ -51,11 +77,12 @@ MrLetsBeFriendsText:
 	line "must be a TRAINER!"
 
 	para "Do you have any"
-	line "#MON you don't want?"
+	line "extra #MON?"
 
 	para "I sure am lonely..."
 
-	para "Say! I have an idea!"
+	para "Say! I have a" 
+	line "good idea!"
 
 	para "Let's be friends!"
 
@@ -73,8 +100,8 @@ Route29FruitTree:
 	jumptext FakeFruitTree
 
 FakeFruitTree:
-	text "I'm not a real fruit"
-	line "tree!"
+	text "I'm not a real"
+	line "fruit tree!"
 
 	para "I'm an imposter!"
 	line "Muhuahahaha!"
@@ -102,11 +129,14 @@ Route29_MapEvents:
 
 	db 0 ; coord events
 
-	db 2 ; bg events
+	db 3 ; bg events
 	bg_event 51,  7, BGEVENT_READ, Route29Sign1
 	bg_event  3,  5, BGEVENT_READ, Route29Sign2
-;	bg_event 53,  2, BGEVENT_ITEM, Route29HiddenItem
+	bg_event 53,  2, BGEVENT_ITEM, Route29HiddenItem
 
-	db 2 ; object events
-	object_event 12,  2, SPRITE_FRUIT_TREE, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route29FruitTree, -1
+	db 5 ; object events
+	object_event 12,  2, SPRITE_FRUIT_TREE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route29FruitTree, -1
 	object_event 25,  4, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MrLetsBeFriends, EVENT_LETS_BE_FRIENDS
+	object_event 53,  3, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Bouncer, -1
+	object_event 57,  4, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BouncerFriend1, -1
+	object_event 55,  2, SPRITE_TWIN, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BouncerFriend2, -1
